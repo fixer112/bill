2a543fad8deef683f0c86d02b02a7a52cb5a40af
Abubakar Lawal <abula3003@gmail.com>
2020-06-16 17:51:23 +0100

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -65,11 +65,11 @@ messaging
             navigator.serviceWorker.register('/firebase-messaging-sw.js')
                 .then(function(registration) {
                     console.log('Registration successful, scope is:', registration.scope);
-                    messaging.setBackgroundMessageHandler(function(payload) {
+                    /* messaging.setBackgroundMessageHandler(function(payload) {
                         console.log('[firebase-messaging-sw.js] Received background message ', payload);
                         // Customize notification here
                         //notifyMe(body, title);
-                    });
+                    }); */
                 }).catch(function(err) {
                     console.log('Service worker registration failed, error:', err);
                 });

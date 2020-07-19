e8488c9948cbda115fd1def3e587a58b80357ba7
Abubakar Lawal <abula3003@gmail.com>
2020-07-19 11:11:14 +0100

    update

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -284,6 +284,7 @@
         <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.18/pdfmake.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/vfs_fonts.min.js"></script>
         <script src="https://cdn.datatables.net/buttons/1.2.4/js/dataTables.buttons.min.js"></script>
         <script src="https://cdn.datatables.net/buttons/1.2.4/js/buttons.bootstrap.js"></script>
         <script src="https://cdn.datatables.net/buttons/1.2.4/js/buttons.html5.js"></script>
